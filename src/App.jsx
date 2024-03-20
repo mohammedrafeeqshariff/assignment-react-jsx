89d4c23e0a51f30b9762e7aee45d73501b9c5cc4
MOHAMMED RAFEEQ <mohammedrafeeqshariff17@gmail.com>
2024-03-20 10:17:35 +0530

    ok boss

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,41 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+
+  return(
+    <>
+
+    <div className='kg'>
+      <h1>Kalvium gallery</h1>
+    </div>
+
+    <div className='images'>
+      {
+        data.map((ele)=>{
+          return <img src={ele.img} alt="elephant image"/>
+        })
+      }
+    </div>
+    </>
+  )
 }
 
 export default App;
